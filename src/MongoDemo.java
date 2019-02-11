0f5849fb6fd608e9f1e46b14f630f97efba80a44
Aleksandr Moshak <moshakalex@gmail.com>
2019-02-11 07:48:48 -0800

    trying GUI

--- src/MongoDemo.java
+++ src/MongoDemo.java
@@ -4,16 +4,17 @@ import com.mongodb.client.MongoDatabase;
 
 public class MongoDemo {
 
-	public static void main(String[] args) {
-		try {
-			MongoClientURI uri = new MongoClientURI("mongodb://alex_user:123qwe@cluster0.mongodb.net/admin");
-			MongoClient mongoClient = new MongoClient();
-			MongoDatabase mongoDB = mongoClient.getDatabase("Csis3275");
-			System.out.println("Connected");
-		}catch (Exception e) {
-			System.out.println(e.toString());
-		}
-
-	}
+//	public static void main(String[] args) {
+//		try {
+//			MongoClientURI uri = new MongoClientURI("mongodb+srv://alex_user:123qwe@cluster0.mongodb.net/admin");
+//			System.out.println(uri);
+//			MongoClient mongoClient = new MongoClient(uri);
+//			MongoDatabase mongoDB = mongoClient.getDatabase("Csis3275");
+//			System.out.println("connected");
+//		}catch (Exception e) {
+//			System.out.println(e.toString());
+//		}
+//
+//	}
 
 }
