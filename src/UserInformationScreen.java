2168bc488daad0b78ee0f3c79330e919924aadd3
patricioGarzon <nicox_g@hotmail.es>
2019-02-24 17:42:06 -0800

    first screen completed, data retrieved from collection in database. if user information is changed database also change

--- src/UserInformationScreen.java
+++ src/UserInformationScreen.java
@@ -2,18 +2,28 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import java.awt.GridBagLayout;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import javax.swing.SwingConstants;
 import javax.swing.text.Document;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 
 import java.awt.Font;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JButton;
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.gt;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class UserInformationScreen  {
 	//region variables
@@ -21,18 +31,21 @@ public class UserInformationScreen  {
 	public JPanel panel;
 	public JLabel lblUserInformation;
 	public JButton btn_EditUserInformation ;
+	public JButton btnSubmit;
 	//labels not to be modified
 	public JLabel lbl_name;
-	public JLabel lbl_Project ;
-	public JLabel lbl_Task;
+	public JLabel lbl_LastName ;
 	public JLabel lbl_Department;
 	//labesl for displaying user information
 	public JLabel lbl_UserName;
-	public JLabel lbl_UserProject;
-	public JLabel lbl_UserTask;
+	public JLabel lbl_UserLastName;
 	public JLabel lbl_UserDepartment;
 	//Mongo Db 
+	public MongoCollection collection;
 	public MongoDemo mongodb;
+	private JTextField textField_FName;
+	private JTextField textField_LName;
+	private JTextField textField_Department;
 	/**
 	 * Launch the application.
 	 */
@@ -68,75 +81,180 @@ public class UserInformationScreen  {
 		frame.setTitle("UserInformation");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
-		frame.setSize(800,600);
+		frame.setSize(600,400);
 		frame.getContentPane().setLayout(null);
 
 		initializeOtherComponents();
 		initializeLabels();
-		
+		setTextBoxesComponentsVisible(false);
 		//retrieve information from user 
 		getUserInformation();
 	}
 	
 	public void getUserInformation() {
-		MongoCollection<Document> collection =  mongodb.getMongoCollection("UserData");
-		Document query = collection.find().first();
-		//System.out.println(query.toJson());
+		collection =  mongodb.getMongoCollection("UserData");
+		org.bson.Document query = (org.bson.Document) collection.find(eq("id",1)).first();
+		List<org.bson.Document> task = (List<org.bson.Document>) query.get("project");
+		System.out.print(task.size());
+		for (org.bson.Document doc : task) {
+			System.out.print(doc.get("name"));
+        }
+		org.bson.Document department = (org.bson.Document) query.get("department");
+		
+		//fill the text boxes with the desired information
+		lbl_UserName.setText(query.get("first_name").toString());
+		lbl_UserLastName.setText(query.get("last_name").toString());
+		lbl_UserDepartment.setText(department.get("name").toString() + " department");
 	}
 	
 	public void initializeLabels(){
 		
 		//labels ui
-		lbl_name = new JLabel("User Name");
+		lbl_name = new JLabel("First Name");
 		lbl_name.setBounds(35, 42, 68, 22);
 		panel.add(lbl_name);
 		
-		lbl_Project = new JLabel("<html>Project <br>\r\n Working on</html>");
-		lbl_Project.setBounds(35, 85, 68, 38);
-		panel.add(lbl_Project);
-		
-		lbl_Task = new JLabel("Tasks");
-		lbl_Task.setBounds(35, 139, 48, 14);
-		panel.add(lbl_Task);
+		lbl_LastName = new JLabel("Last Name");
+		lbl_LastName.setBounds(35, 104, 68, 38);
+		panel.add(lbl_LastName);
 		
 		lbl_Department = new JLabel("Department");
-		lbl_Department.setBounds(35, 176, 48, 14);
+		lbl_Department.setBounds(35, 176, 68, 23);
 		panel.add(lbl_Department);
 		
 		//for displaying user information
 		lbl_UserName = new JLabel("New label");
-		lbl_UserName.setBounds(164, 46, 48, 14);
+		lbl_UserName.setBounds(164, 46, 111, 18);
 		panel.add(lbl_UserName);
 		
-		lbl_UserProject = new JLabel("New label");
-		lbl_UserProject.setBounds(164, 101, 48, 14);
-		panel.add(lbl_UserProject);
-		
-		lbl_UserTask = new JLabel("New label");
-		lbl_UserTask.setBounds(164, 139, 48, 14);
-		panel.add(lbl_UserTask);
+		lbl_UserLastName = new JLabel("New label");
+		lbl_UserLastName.setBounds(164, 114, 111, 18);
+		panel.add(lbl_UserLastName);
 		
 		lbl_UserDepartment = new JLabel("New label");
-		lbl_UserDepartment.setBounds(164, 176, 48, 14);
+		lbl_UserDepartment.setBounds(166, 176, 97, 23);
 		panel.add(lbl_UserDepartment);
+		
+		btnSubmit = new JButton("Submit");
+		btnSubmit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//check if texbox is not empty
+				if(textBoxesVerification()) {
+					System.out.print("true");
+					//get values from textboxes into labels
+					setLabelUpdatedText(textField_FName.getText(),textField_LName.getText(),textField_Department.getText());
+					setLabelComponentsVisible(true);
+					setTextBoxesComponentsVisible(false);
+					btnSubmit.setVisible(false);
+					btn_EditUserInformation.setVisible(true);
+					// uptdate datbase with values
+					updateDatabse();
+					
+				}else {
+					JOptionPane.showMessageDialog(null, "all fields must be completeed");
+					System.out.print("false");
+				}				
+			}
+		});
+		btnSubmit.setBounds(174, 244, 89, 23);
+		panel.add(btnSubmit);
+		btnSubmit.setVisible(false);
+		
 
+
+	}
+	
+	public void updateDatabse() {
+		collection.updateOne(eq("id", 1), new org.bson.Document("$set", new org.bson.Document("first_name", textField_FName.getText())));
+		collection.updateOne(eq("id", 1), new org.bson.Document("$set", new org.bson.Document("last_name", textField_LName.getText())));
+		collection.updateOne(eq("id", 1), new org.bson.Document("$set", new org.bson.Document("department.name", textField_Department.getText())));
+	}
+	
+	public boolean textBoxesVerification() {
+		if( !textField_FName.getText().isEmpty() ) {
+			if(!textField_LName.getText().isEmpty() ) {
+				if(!textField_Department.getText().isEmpty() ) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	public void setLabelUpdatedText(String name, String lastName, String department) {
+		lbl_UserName.setText(name);
+		lbl_UserLastName.setText(lastName);
+		lbl_UserDepartment.setText(department);
 	}
 	
 	public void initializeOtherComponents() {
 		lblUserInformation = new JLabel("User Information");
 		lblUserInformation.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblUserInformation.setBounds(332, 56, 128, 31);
+		lblUserInformation.setBounds(232, 11, 128, 31);
 		frame.getContentPane().add(lblUserInformation);
 		
 		panel = new JPanel();
-		panel.setBounds(245, 135, 305, 278);
+		panel.setBounds(134, 53, 305, 278);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
+		textField_FName = new JTextField();
+		textField_FName.setBounds(164, 43, 96, 20);
+		panel.add(textField_FName);
+		textField_FName.setColumns(10);
+
+		
+		textField_LName = new JTextField();
+		textField_LName.setBounds(164, 113, 96, 20);
+		panel.add(textField_LName);
+		textField_LName.setColumns(10);
+		
+		textField_Department = new JTextField();
+		textField_Department.setBounds(164, 177, 96, 20);
+		panel.add(textField_Department);
+		textField_Department.setColumns(10);
+		
+		
 		btn_EditUserInformation = new JButton("Edit information");
+		btn_EditUserInformation.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// handle new window creation here
+				setLabelComponentsVisible(false);
+				setTextBoxesComponentsVisible(true);
+				btnSubmit.setVisible(true);
+				btn_EditUserInformation.setVisible(false);
+			}
+		});
+		
+		
+		/*task button should open an new page and close previouse one, interface needed for handling all screens
+		 * btn_EditUserInformation.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// handle new window creation here
+				LoginScreen screen  = new LoginScreen();
+				EventQueue.invokeLater(new Runnable() {
+			        public void run() 
+			        {
+			           //inititate third screen and set make second screen invisible
+			        	new LoginScreen().setVisible(true);
+			        	frame.setVisible(false);
+			        	frame.dispose();
+			        }
+				});
+			}
+		});*/
 		btn_EditUserInformation.setBounds(164, 219, 111, 23);
 		panel.add(btn_EditUserInformation);
 	}
-	
 
+	public void setLabelComponentsVisible(boolean bool) {
+		lbl_UserName.setVisible(bool);
+		lbl_UserLastName.setVisible(bool);
+		lbl_UserDepartment.setVisible(bool);
+	}
+	
+	public void setTextBoxesComponentsVisible(boolean bool) {
+		textField_FName.setVisible(bool);
+		textField_LName.setVisible(bool);
+		textField_Department.setVisible(bool);
+	}
 }
